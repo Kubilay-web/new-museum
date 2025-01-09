09c845e6c4ea335f7f852c506a33c8dd539c4356
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-01-09 17:15:26 +0300

    Updated

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -34,6 +34,7 @@ function Admin() {
     <div>
       <Helmet>
         <link rel="stylesheet" href="css/sb-admin-2.min.css" />
+        <link rel="stylesheet" href="css/sb-admin-2.min.css" />
         <link
           href="vendor/fontawesome-free/css/all.min.css"
           rel="stylesheet"
@@ -42,14 +43,7 @@ function Admin() {
         <script src="vendor/jquery/jquery.min.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
         <script src="js/sb-admin-2.min.js"></script>
-
-        <script src="vendor/chart.js/Chart.min.js"></script>
-
-        <script src="js/demo/chart-area-demo.js"></script>
-        <script src="js/demo/chart-pie-demo.js"></script>
       </Helmet>
       <body id="page-top">
         {/*  <!-- Page Wrapper --> */}
