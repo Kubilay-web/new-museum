643537e6b77691c69b629699835004bf0b03b1e2
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-01-14 14:19:08 +0300

    Updated

--- backend/routes/subscribeRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import subscribeController from "../controllers/subscribeController.js";
-
-const router = express.Router();
-
-// POST isteği: /api/subscribe
-router.post("/", subscribeController);
-
-export default router;
